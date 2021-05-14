383db66f0e7f8d193ded1b830710ea9edbaacfe3
Thays <thays.santos@omeletecompany.com>
2021-05-14 15:05:34 -0300

    Desafio 2 - Componentizando aplicação

---
+++ src/components/Header.tsx
@@ -0,0 +1,14 @@
+
+interface HeaderProps{
+  selectedGenre: {
+    title: string
+  }
+}
+
+export function Header(props: HeaderProps) {
+  return (
+    <header>
+      <span className="category">Categoria:<span> {props.selectedGenre.title}</span></span>
+    </header>
+  )
+}
